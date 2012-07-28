9d028a0817060be62b050d8fe229b09ae1c8d347
Igor Tomych <igor.tomich@gmail.com>
2012-07-28 12:16:53 +0300

    UI fixes

--- Lecture5/Lecture5/AppDelegate.m
+++ Lecture5/Lecture5/AppDelegate.m
@@ -23,9 +23,15 @@
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
+
     self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
-    self.window.rootViewController = self.viewController;
+    UINavigationController* navController = [[UINavigationController alloc] initWithRootViewController:self.viewController];
+    
+    self.window.rootViewController = navController;
+    
     [self.window makeKeyAndVisible];
+//    [UIApplication sharedApplication].statusBarHidden = NO;
+    
     return YES;
 }
 
