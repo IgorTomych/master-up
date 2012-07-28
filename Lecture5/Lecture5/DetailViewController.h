93c185d20ad23f55e641aee251d2707c69d33121
Igor Tomych <igor.tomich@gmail.com>
2012-07-28 13:05:14 +0300

    iPad version update

---
+++ Lecture5/Lecture5/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Lecture5
+//
+//  Created by Igor Tomych on 7/28/12.
+//  Copyright (c) 2012 Igor Tomych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ Lecture5/Lecture5/DetailViewController.m
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.m
+//  Lecture5
+//
+//  Created by Igor Tomych on 7/28/12.
+//  Copyright (c) 2012 Igor Tomych. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
